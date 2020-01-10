20e4bd71cb72b74e911ae3117fe6556c7177b3cb
yusaku <gpa.12512@gmail.com>
2020-01-10 16:41:25 +0900

    編集機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,20 @@ class PostsController < ApplicationController
     def show 
         @post = Post.find(params[:id])
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        post = Post.find(params[:id])
+        post.update(post_params)
+        redirect_to root_path
+    end
+
+    def destroy_confirm
+        @post = Post.find(params[:id])
+    end
     
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "top#index"
-  resources :posts, except: :index
+  resources :posts, except: :index do 
+    member do
+      get :destroy_confirm
+    end
+  end
   resources :users do 
     resources :posts, only: :index
   end
